e9bfd064a3809a21087df7d27e3c1552b491b23f
Daniel Gonçalves Lopes <danielopesgl05@gmail.com>
2022-04-28 17:36:19 -0300

    feat: ensure Service returns SignUpUserError when LoadUserByEmail returns undefined

--- tests/unit/data/services/signup-user.spec.ts
+++ tests/unit/data/services/signup-user.spec.ts
@@ -1,30 +1,42 @@
+import { SignUpUserError } from '@/domain/errors';
 import { SignUpUser } from '@/domain/features';
 
 class SignUpUserService {
   constructor(private readonly loadUserByEmail: LoadUserByEmailRepository) {}
 
-  async perform(params: SignUpUser.Params): Promise<void> {
+  async perform(params: SignUpUser.Params): Promise<SignUpUser.Result> {
     const { email } = params;
 
     await this.loadUserByEmail.loadByEmail({ email });
+
+    return new SignUpUserError();
   }
 }
 
 interface LoadUserByEmailRepository {
-  loadByEmail: (params: LoadUserByEmailRepository.Params) => Promise<void>;
+  loadByEmail: (
+    params: LoadUserByEmailRepository.Params,
+  ) => Promise<LoadUserByEmailRepository.Result>;
 }
 
 namespace LoadUserByEmailRepository {
   export type Params = {
     email: string;
   };
+
+  export type Result = undefined;
 }
 
 class LoadUserByEmailSpy implements LoadUserByEmailRepository {
   email?: string;
+  result = undefined;
 
-  async loadByEmail(params: LoadUserByEmailRepository.Params): Promise<void> {
+  async loadByEmail(
+    params: LoadUserByEmailRepository.Params,
+  ): Promise<LoadUserByEmailRepository.Result> {
     this.email = params.email;
+
+    return this.result;
   }
 }
 
@@ -41,5 +53,21 @@ describe('SignUpUserService', () => {
     });
 
     expect(loadUserByEmail.email).toBe('any_email');
+  });
+
+  it('should return authentication error when LoadUserByEmail returns undefined', async () => {
+    const loadUserByEmail = new LoadUserByEmailSpy();
+
+    loadUserByEmail.result = undefined;
+
+    const sut = new SignUpUserService(loadUserByEmail);
+
+    const signUpResult = await sut.perform({
+      name: 'any_name',
+      email: 'any_email',
+      password: 'any_password',
+    });
+
+    expect(signUpResult).toEqual(new SignUpUserError());
   });
 });
