561358c895620abddde896aba818f7d323d06161
Daniel Gonçalves Lopes <danielopesgl05@gmail.com>
2022-04-28 18:06:52 -0300

    refactor: simplify code

--- tests/unit/data/services/signup-user.spec.ts
+++ tests/unit/data/services/signup-user.spec.ts
@@ -1,38 +1,34 @@
 import { LoadUserByEmailRepository } from '@/data/contracts/repositories';
 import { SignUpUserService } from '@/data/services';
 import { SignUpUserError } from '@/domain/errors';
-import { mock } from 'jest-mock-extended';
+import { mock, MockProxy } from 'jest-mock-extended';
 
 describe('SignUpUserService', () => {
-  it('should call LoadUserByEmail with correct params', async () => {
-    const loadUserByEmail = mock<LoadUserByEmailRepository>();
+  let loadUserByEmailRepository: MockProxy<LoadUserByEmailRepository>;
+  let sut: SignUpUserService;
+  const userDatas = {
+    name: 'any_name',
+    email: 'any_email',
+    password: 'any_password',
+  };
 
-    const sut = new SignUpUserService(loadUserByEmail);
+  beforeEach(() => {
+    loadUserByEmailRepository = mock();
 
-    await sut.perform({
-      name: 'any_name',
-      email: 'any_email',
-      password: 'any_password',
-    });
-
-    expect(loadUserByEmail.loadByEmail).toHaveBeenCalledWith({
-      email: 'any_email',
-    });
-    expect(loadUserByEmail.loadByEmail).toHaveBeenCalledTimes(1);
+    sut = new SignUpUserService(loadUserByEmailRepository);
   });
 
-  it('should return authentication error when LoadUserByEmail returns undefined', async () => {
-    const loadUserByEmail = mock<LoadUserByEmailRepository>();
+  it('should call LoadUserByEmailRepository with correct params', async () => {
+    await sut.perform(userDatas);
 
-    loadUserByEmail.loadByEmail.mockResolvedValueOnce(undefined);
-
-    const sut = new SignUpUserService(loadUserByEmail);
-
-    const signUpResult = await sut.perform({
-      name: 'any_name',
+    expect(loadUserByEmailRepository.loadByEmail).toHaveBeenCalledWith({
       email: 'any_email',
-      password: 'any_password',
     });
+    expect(loadUserByEmailRepository.loadByEmail).toHaveBeenCalledTimes(1);
+  });
+
+  it('should return authentication error when LoadUserByEmailRepository returns undefined', async () => {
+    const signUpResult = await sut.perform(userDatas);
 
     expect(signUpResult).toEqual(new SignUpUserError());
   });
