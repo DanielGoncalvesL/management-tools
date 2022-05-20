0338b228a225aebc2bc0744d82c3aaac44fff7c0
Daniel Gonçalves Lopes <danielopesgl05@gmail.com>
2022-05-20 10:00:38 -0300

    chore: fix path for application entities

--- ormconfig.js
+++ ormconfig.js
@@ -1,13 +1,12 @@
 const root = process.env.TS_NODE_DEV === undefined ? 'dist' : 'src'
 
-
 module.exports = {
     type: 'postgres',
     host: process.env.POSTGRESQL_HOST,
     username: process.env.POSTGRESQL_USERNAME,
     password: process.env.POSTGRESQL_PASSWORD,
     database: process.env.POSTGRESQL_DATABASE,
-    entities: [`./${root}/infra/repositories/entities/**/*.{js,ts}`],
+    entities: [`./${root}/infra/repositories/postgres/entities/*.{js,ts}`],
     // migrations: [`./${root}/infra/db/typeorm/migrations/*.{js,ts}`],
     // cli: {
     //     "migrationsDir": `./${root}/infra/db/typeorm/migrations/`
