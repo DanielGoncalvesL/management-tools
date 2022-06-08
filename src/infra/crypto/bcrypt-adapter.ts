d2b726ca160896963b47cd382f8bf5a1dde4e906
Daniel Gonçalves Lopes <danielopesgl05@gmail.com>
2022-06-08 01:35:15 -0300

    feat: implement HashComparer with bcrypt

--- src/infra/crypto/bcrypt-adapter.ts
+++ src/infra/crypto/bcrypt-adapter.ts
@@ -1,10 +1,17 @@
-import { Hasher } from '@/domain/contracts/providers';
+import { HashComparer, Hasher } from '@/domain/contracts/providers';
 import bcrypt from 'bcrypt';
 
-export class BcryptAdapter implements Hasher {
+export class BcryptAdapter implements Hasher, HashComparer {
   constructor(private readonly salt: number) {}
 
   async hash({ plaintext }: Hasher.Params): Promise<Hasher.Result> {
     return bcrypt.hash(plaintext, this.salt);
+  }
+
+  async compare({
+    plaintext,
+    textToCompare,
+  }: HashComparer.Params): Promise<HashComparer.Result> {
+    return bcrypt.compare(plaintext, textToCompare);
   }
 }
--- tests/unit/infra/crypto/bcrypt-adapter.spec.ts
+++ tests/unit/infra/crypto/bcrypt-adapter.spec.ts
@@ -18,29 +18,70 @@ describe('BcryptAdapter', () => {
     fakeBcrypt = bcrypt as jest.Mocked<typeof bcrypt>;
 
     fakeBcrypt.hash.mockImplementation(() => 'any_hashed_value');
+    fakeBcrypt.compare.mockImplementation(() => true);
   });
 
   beforeEach(() => {
     sut = new BcryptAdapter(salt);
   });
+  describe('hash', () => {
+    it('should call sign with correct params', async () => {
+      await sut.hash(hashData);
 
-  it('should call sign with correct params', async () => {
-    await sut.hash(hashData);
+      expect(fakeBcrypt.hash).toHaveBeenCalledWith(hashData.plaintext, salt);
+    });
 
-    expect(fakeBcrypt.hash).toHaveBeenCalledWith(hashData.plaintext, salt);
+    it('should return a hashed value', async () => {
+      const hashed = await sut.hash(hashData);
+
+      expect(hashed).toBe('any_hashed_value');
+    });
+
+    it('should throw if hash throws', async () => {
+      fakeBcrypt.hash.mockImplementationOnce(throwError);
+
+      const promise = sut.hash(hashData);
+
+      await expect(promise).rejects.toThrow();
+    });
   });
 
-  it('should return a hashed value', async () => {
-    const hashed = await sut.hash(hashData);
+  describe('compare', () => {
+    const compareDatas = {
+      plaintext: 'any_password',
+      textToCompare: 'hashed_password',
+    };
 
-    expect(hashed).toBe('any_hashed_value');
-  });
+    it('should call compare with correct params', async () => {
+      await sut.compare(compareDatas);
 
-  it('should throw if hash throws', async () => {
-    fakeBcrypt.hash.mockImplementationOnce(throwError);
+      expect(fakeBcrypt.compare).toHaveBeenCalledTimes(1);
+      expect(fakeBcrypt.compare).toHaveBeenCalledWith(
+        compareDatas.plaintext,
+        compareDatas.textToCompare,
+      );
+    });
 
-    const promise = sut.hash(hashData);
+    it('should return true if compare success', async () => {
+      const compare = await sut.compare(compareDatas);
 
-    await expect(promise).rejects.toThrow();
+      expect(compare).toBeTruthy();
+    });
+
+    it('should return false if compare fails', async () => {
+      fakeBcrypt.compare.mockImplementationOnce(() => false);
+
+      const compare = await sut.compare(compareDatas);
+
+      expect(compare).toBeFalsy();
+    });
+
+    it('should throw if compare throws', async () => {
+      fakeBcrypt.compare.mockImplementationOnce(throwError);
+
+      const promise = sut.compare(compareDatas);
+
+      await expect(promise).rejects.toThrow();
+    });
   });
 });
