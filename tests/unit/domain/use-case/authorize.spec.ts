f01caafa105d5d06d8750adb9e439c4942a8a8b0
Daniel Gonçalves <danielopesgl05@gmail.com>
2022-07-14 01:13:41 -0300

    chore: ensure Authorize calls TokenValidator with correct params

---
+++ tests/unit/domain/use-case/authorize.spec.ts
@@ -0,0 +1,57 @@
+import { mock, MockProxy } from 'jest-mock-extended';
+
+export interface TokenValidator {
+  validate(token: TokenValidator.Params): Promise<void>;
+}
+
+export namespace TokenValidator {
+  export type Params = { token: string };
+}
+
+export interface CheckUserById {
+  checkById(id: CheckUserById.Params): Promise<CheckUserById.Result>;
+}
+
+export namespace CheckUserById {
+  export type Params = { id: string };
+
+  export type Result = boolean;
+}
+
+type Setup = (
+  tokenValidator: TokenValidator,
+  userRepository: CheckUserById,
+) => Authorize;
+
+type Authorize = (params: { token: string }) => Promise<void>;
+
+const setupAuthorize: Setup =
+  (tokenValidator, userRepository) => async params => {
+    await tokenValidator.validate(params);
+  };
+
+describe('Authorize', () => {
+  let userRepository: MockProxy<CheckUserById>;
+  let tokenValidator: MockProxy<TokenValidator>;
+  let sut: Authorize;
+
+  beforeEach(() => {
+    userRepository = mock();
+    tokenValidator = mock();
+
+    userRepository.checkById.mockResolvedValue(true);
+
+    tokenValidator.validate.mockResolvedValue();
+
+    sut = setupAuthorize(tokenValidator, userRepository);
+  });
+
+  it('should call TokenValidator with correct params', async () => {
+    await sut({ token: 'any_token' });
+
+    expect(tokenValidator.validate).toHaveBeenCalledWith({
+      token: 'any_token',
+    });
+    expect(tokenValidator.validate).toHaveBeenCalledTimes(1);
+  });
+});
