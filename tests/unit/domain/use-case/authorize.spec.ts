848c9b10ac294331bb394595ca9f4d7a884ba934
Daniel Gonçalves <danielopesgl05@gmail.com>
2022-07-14 01:25:06 -0300

    chore: ensure Authorize throw UnauthorizedError if TokenValidator returns false

--- tests/unit/domain/use-case/authorize.spec.ts
+++ tests/unit/domain/use-case/authorize.spec.ts
@@ -1,12 +1,15 @@
 import { throwError } from '@/../tests/mocks';
+import { UnauthorizedError } from '@/application/errors';
 import { mock, MockProxy } from 'jest-mock-extended';
 
 export interface TokenValidator {
-  validate(token: TokenValidator.Params): Promise<void>;
+  validate(token: TokenValidator.Params): Promise<TokenValidator.Result>;
 }
 
 export namespace TokenValidator {
   export type Params = { token: string };
+
+  export type Result = { id: string } | false;
 }
 
 export interface CheckUserById {
@@ -28,7 +31,11 @@ type Authorize = (params: { token: string }) => Promise<void>;
 
 const setupAuthorize: Setup =
   (tokenValidator, userRepository) => async params => {
-    await tokenValidator.validate(params);
+    const token = await tokenValidator.validate(params);
+
+    if (!token) {
+      throw new UnauthorizedError();
+    }
   };
 
 describe('Authorize', () => {
@@ -36,31 +43,39 @@ describe('Authorize', () => {
   let tokenValidator: MockProxy<TokenValidator>;
   let sut: Authorize;
 
+  const sutData = { token: 'any_token' };
+
   beforeEach(() => {
     userRepository = mock();
     tokenValidator = mock();
 
     userRepository.checkById.mockResolvedValue(true);
 
-    tokenValidator.validate.mockResolvedValue();
+    tokenValidator.validate.mockResolvedValue({ id: 'any_token' });
 
     sut = setupAuthorize(tokenValidator, userRepository);
   });
 
   it('should call TokenValidator with correct params', async () => {
-    await sut({ token: 'any_token' });
+    await sut(sutData);
 
-    expect(tokenValidator.validate).toHaveBeenCalledWith({
-      token: 'any_token',
-    });
+    expect(tokenValidator.validate).toHaveBeenCalledWith(sutData);
     expect(tokenValidator.validate).toHaveBeenCalledTimes(1);
   });
 
   it('should throw if TokenValidator throws', async () => {
     tokenValidator.validate.mockImplementationOnce(throwError);
 
-    const promise = sut({ token: 'any_token' });
+    const promise = sut(sutData);
 
     await expect(promise).rejects.toThrow();
+  });
+
+  it('should throw if TokenValidator returns false', async () => {
+    tokenValidator.validate.mockResolvedValueOnce(false);
+
+    const promise = sut(sutData);
+
+    await expect(promise).rejects.toThrow(new UnauthorizedError());
   });
 });
